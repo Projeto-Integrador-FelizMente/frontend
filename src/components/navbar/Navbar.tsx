da7ff8b42274e41b4dbc1ab1b79b8f00ed7f83c4
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-10-30 12:36:07 -0300

    import

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,41 +1,39 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
-      <div className='w-full bg-indigo-700 text-white
-              flex justify-center py-4'>
-
+      <div
+        className="w-full bg-indigo-700 text-white
+              flex justify-center py-4"
+      >
         <div className="container flex justify-between text-lg">
           FelizMente
-
-          <div className='flex gap-4'>
-            <Link to='./home' className="text-indigo-800 hover:underline">
+          <div className="flex gap-4">
+            <Link to="./home" className="text-indigo-800 hover:underline">
               Postagens
             </Link>
 
-            <Link to='./home' className="text-indigo-800 hover:underline">
+            <Link to="./home" className="text-indigo-800 hover:underline">
               Temas
             </Link>
 
-            <Link to='./home' className="text-indigo-800 hover:underline">
+            <Link to="./home" className="text-indigo-800 hover:underline">
               Cadastrar tema
             </Link>
 
-            <Link to='./home' className="text-indigo-800 hover:underline">
+            <Link to="./home" className="text-indigo-800 hover:underline">
               Perfil
             </Link>
 
-            <Link to='./login' className="text-indigo-800 hover:underline">
+            <Link to="./login" className="text-indigo-800 hover:underline">
               Sair
             </Link>
-
-
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
