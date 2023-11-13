441977e0f654c30cc8763886e9dadcd6c5704c16
Mateus Siqueira Salomão <118471492+mateusSiqueira2004@users.noreply.github.com>
2023-11-13 14:57:07 -0300

    Perfil Melhorado

--- src/pages/perfil/Perfil.tsx
+++ src/pages/perfil/Perfil.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { AuthContext } from '../../contexts/AuthContext'
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens'
 
 function Perfil() {
 
@@ -17,25 +18,31 @@ function Perfil() {
     }, [usuario.token])
 
     return (
-        <div className='container mx-auto mt-4 rounded-2xl overflow-hidden'>
+        <>
+            <div className='flex'>
+                
+                <div className='container rounded-2xl overflow-hidden w-1/4 h-screen bg-sky-500 '>
 
-            <img 
-                className='w-full h-72 object-cover border-b-8 border-white' 
-                src="https://i.imgur.com/ZZFAmzo.jpg" alt="Capa do Perfil" />
+                    <img
+                        className='rounded-full w-56 h-56 mx-auto text-center  border-slate-900 border justify-center'
+                        src={usuario.foto} alt={`Foto de perfil de ${usuario.nome}`} />
 
-            <img 
-                className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' 
-                src={usuario.foto} alt={`Foto de perfil de ${usuario.nome}`} />
+                    <div
+                        className="  flex flex-col
+                     text-white text-2xl items-center justify-center"
+                    >
+                        <p>Nome: {usuario.nome} </p>
+                        <p>Email: {usuario.usuario}</p>
+                        <p>Tipo: {usuario.tipo}</p>
+                    </div>
 
-            <div 
-                className="relative mt-[-6rem] h-72 flex flex-col 
-                    bg-sky-500 text-white text-2xl items-center justify-center"
-            >
-                <p>Nome: {usuario.nome} </p>
-                <p>Email: {usuario.usuario}</p>
+                </div>
+                <div className='mx-auto text-center ml-2'>
+                    <h1 className='text-xl font-bold underline'>Minhas Postagens</h1>
+                    <ListaPostagens/>
+                </div>
             </div>
-
-        </div>
+        </>
     )
 }
 
