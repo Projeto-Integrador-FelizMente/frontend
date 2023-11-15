0a213523d1cf741d4d11ea0856c5c91f8443fd2d
gabrielsopimentel <gabrielsopimentel@gmail.com>
2023-11-15 02:12:53 -0300

    fix contato

--- src/pages/contato/Contato.tsx
+++ src/pages/contato/Contato.tsx
@@ -3,26 +3,63 @@ import CardContato from "../../components/contato/cardContato/CardContato";
 function Contato() {
   const contatos = [
     { nome: "Gabriel", linkedin: "linkedin.com", github: "github.com" },
-    { id: "lucas", nome: "Lucas", linkedin: "linkedin.com", github: "github.com" },
-    { id: "Mateus", nome: "Mateus", linkedin: "linkedin.com", github: "github.com" },
-    { id: "pedroA", nome: "Pedro A", linkedin: "linkedin.com", github: "github.com" },
-    { id: "pedroM", nome: "Pedro M", linkedin: "linkedin.com", github: "github.com" },
-    { id: "vitor", nome: "Vitor", linkedin: "linkedin.com", github: "github.com" },
-    { id: "felizmente", nome: "FelizMente", linkedin: "linkedin.com", github: "github.com" },
+    {
+      nome: "Lucas",
+      linkedin: "linkedin.com",
+      github: "github.com",
+    },
+    {
+      nome: "Mateus",
+      linkedin: "linkedin.com",
+      github: "github.com",
+    },
+    {
+      nome: "Pedro A",
+      linkedin: "linkedin.com",
+      github: "github.com",
+    },
+    {
+      nome: "Pedro M",
+      linkedin: "linkedin.com",
+      github: "github.com",
+    },
+    {
+      nome: "Vitor",
+      linkedin: "linkedin.com",
+      github: "github.com",
+    },
+  ];
+
+  const contats = [
+    {
+      nome: "FelizMente",
+      linkedin: "linkedin.com",
+      github: "github.com",
+    },
   ];
 
   return (
-    <div className="container ">
+    <div>
       <h1 className="text-4xl font-bold text-center">Contatos</h1>
-        <div className="grid grid-cols-6 gap-4 mx-auto p-3">
+      <div className="mx-auto w-max flex">
+        <div className="grid grid-cols-6 gap-4">
           {contatos.map((contato) => (
             <CardContato
-              key={contato.id}
               nome={contato.nome}
               linkedin={contato.linkedin}
               github={contato.github}
             />
           ))}
+        </div>
+      </div>
+      <div className="mx-auto w-max">
+        {contats.map((contato) => (
+          <CardContato
+            nome={contato.nome}
+            linkedin={contato.linkedin}
+            github={contato.github}
+          />
+        ))}
       </div>
     </div>
   );
