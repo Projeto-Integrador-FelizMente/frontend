2c8bd4a087a054516bad36b46432fda48f190f2f
Mateus Siqueira Salomão <118471492+mateusSiqueira2004@users.noreply.github.com>
2023-11-17 08:43:13 -0300

    correção modo Dark

--- src/components/temas/formularioTema/FormularioTema.tsx
+++ src/components/temas/formularioTema/FormularioTema.tsx
@@ -124,7 +124,7 @@ function FormularioTema() {
             type="text"
             placeholder="Descreva aqui seu tema"
             name="descricao"
-            className="border-2 border-slate-700 rounded p-2"
+            className="border-2 dark:border-slate-200 dark:bg-black border-slate-700 rounded p-2"
             value={tema.descricao}
             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
           />
